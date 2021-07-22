8692901078e481b7f02b7ea1588b0f804c0bd1df
Dann Luciano <dannluciano@gmail.com>
2021-07-22 19:55:30 -0300

    Fix Hint DEFAULT_AUTO_FIELD

--- miniexplorer/apps.py
+++ miniexplorer/apps.py
@@ -2,4 +2,7 @@ from django.apps import AppConfig
 
 
 class MiniexplorerConfig(AppConfig):
-    name = "miniexplorer"
+    name = 'miniexplorer'
+    verbose_name = 'SQL Explorer'
+    
+    default_auto_field = 'django.db.models.AutoField'
